ee00303b5bfb7269f5f66c1b1f9a65c9c1b7a864
dgotlieb <Dgotlieb8@gmail.com>
2022-08-13 15:17:36 +0300

    Delete webtest.py

--- webtest.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-# Windows:
-driver = webdriver.Chrome(executable_path="C:\\Users\user\Desktop\ChromeDriver.exe")
-
-# Mac and linux:
-driver = webdriver.Chrome(executable_path="/users/user/chromedriver")
